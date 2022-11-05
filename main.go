2e2c4accd2615ae014f3a0c1db3c185106527aa3
jackhydez <pash.ok92@yandex.ru>
2022-11-05 23:56:37 +0300

    test jenkins 03

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-    w.Write([]byte("hello world 556"))
+    w.Write([]byte("hello world 777"))
 }
 
 func main() {
-    fmt.Println("hello")
+    fmt.Println("hello 1")
     router := httprouter.New()
     router.GET("/", IndexHandler)
     log.Fatal(http.ListenAndServe(":9091", router))
